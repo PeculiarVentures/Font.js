2aaf79c1067c2ef8c665e8153dbbe9ed56038553
microshine <microshine@mail.ru>
2021-04-02 14:31:51 +0300

    refactor: Update export

--- src/index.ts
+++ src/index.ts
@@ -1,104 +1,56 @@
-import * as exp from "./export";
-
-function fix(obj: any) {
-  for (const key in obj) {
-    const data = obj[key];
-
-    if (typeof data === "function") {
-      if (data.prototype) {
-        // class
-
-        // static
-        {
-          const methods = Object.getOwnPropertyNames(data);
-          for (const item of methods) {
-            const descriptor = Object.getOwnPropertyDescriptor(data, item);
-            if (descriptor && (descriptor.set || descriptor.get)) {
-              if (descriptor.get) {
-                const get = descriptor.get;
-                console.log(`Wrap ${data.name}::${item} (get) static`);
-                descriptor.get = function (this: any) {
-                  console.log(`${data.name}::${item} (get) static`);
-
-                  return get.call(this);
-                };
-              }
-              if (descriptor.set) {
-                const set = descriptor.set;
-                console.log(`Wrap ${data.name}::${item} (set) static`);
-                descriptor.set = function (this: any, value: any) {
-                  console.log(`${data.name}::${item} (set) static`);
-
-                  return set.call(this, value);
-                };
-              }
-              continue;
-            }
-            const method = data[item];
-            if (typeof method === "function") {
-              if (item !== "constructor")
-                console.log(`Wrap ${data.name}::${item} static`);
-              data[item] = function (this: any, ...args: any[]) {
-                console.log(`${data.name}::${item}`);
-
-                return method.call(this, ...args);
-              };
-            }
-          }
-        }
-
-        // prototype
-        const methods = Object.getOwnPropertyNames(data.prototype);
-        for (const item of methods) {
-          const descriptor = Object.getOwnPropertyDescriptor(data.prototype, item);
-          if (descriptor && (descriptor.set || descriptor.get)) {
-            if (descriptor.get) {
-              const get = descriptor.get;
-              console.log(`Wrap ${data.name}::${item} (get)`);
-              descriptor.get = function (this: any) {
-                console.log(`${data.name}::${item} (get)`);
-
-                return get.call(this);
-              };
-            }
-            if (descriptor.set) {
-              const set = descriptor.set;
-              console.log(`Wrap ${data.name}::${item} (set)`);
-              descriptor.set = function (this: any, value: any) {
-                console.log(`${data.name}::${item} (set)`);
-
-                return set.call(this, value);
-              };
-            }
-            continue;
-          }
-          const method = data.prototype[item];
-          if (typeof method === "function") {
-            if (item !== "constructor")
-              console.log(`Wrap ${data.name}::${item}`);
-            data.prototype[item] = function (this: any, ...args: any[]) {
-              console.log(`${data.name}::${item}`);
-
-              return method.call(this, ...args);
-            };
-          }
-        }
-      } else {
-        console.log(`Wrap ${key}`);
-        obj[key] = function (...args: any[]) {
-          console.log(`${key}`);
-
-          return data.call(this, ...args);
-        };
-      }
-    } else if (typeof data === "object") {
-      // namespace
-      fix(data);
-    }
-
-  }
-}
-
-// fix(exp);
-
-export * from "./export";
+import {
+	checkFlag,
+	appendF2Dot14,
+	getF2Dot14,
+	getLongDateTime,
+	appendLongDateTime,
+	stringToCodePoints,
+	stringToUnicode,
+	stringToUnicodeHex,
+	unicodePointsToCodePoints,
+	unicodeToString,
+	unicodeHexToString,
+	appendFixed
+} from "./common";
+export {
+	checkFlag,
+	appendF2Dot14,
+	getF2Dot14,
+	getLongDateTime,
+	appendLongDateTime,
+	stringToCodePoints,
+	stringToUnicode,
+	stringToUnicodeHex,
+	unicodePointsToCodePoints,
+	unicodeToString,
+	unicodeHexToString,
+	appendFixed
+};
+import {
+	Matrix
+} from "./Matrix";
+export {
+	Matrix
+};
+import {
+	BaseClass
+} from "./BaseClass";
+export {
+	BaseClass
+};
+import * as Tables from "./tables";
+export {
+	Tables
+};
+import {
+	ScalerTypes,
+	Font
+} from "./Font";
+export {
+	ScalerTypes,
+	Font
+};
+import { FontCollection } from "./FontCollection";
+export {
+	FontCollection
+};
