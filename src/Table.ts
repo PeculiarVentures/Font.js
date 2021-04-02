e04686a17c78bfe78911d3a280226b25ddc32947
microshine <microshine@mail.ru>
2021-04-02 14:28:10 +0300

    docs: Add description

--- src/Table.ts
+++ src/Table.ts
@@ -2,6 +2,9 @@ import { BaseClass } from "./BaseClass";
 
 export class FontTable extends BaseClass {
 
+  /**
+   * Returns tag for the font table
+   */
   public static get tag() {
     return 0;
   }
